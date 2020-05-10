c63a0dfd2245dd9e88588a4f510b0d2b1c9b6c41
Webdozen <microlion116@gmail.com>
2020-05-10 10:23:34 +0300

    user login, signup

---
+++ commit.sh
@@ -0,0 +1,12 @@
+Y=2020
+M=5
+D=10
+i=23
+comment="user login, signup"
+
+export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
+export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
+# git add commit.md -f
+git add .
+git commit --date="$Y-$M-$D 10:$i:34" -m "$comment"
+git push
