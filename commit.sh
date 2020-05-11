b4bbece1817ccd7df528e08dcd6d913ff763b031
Webdozen <microlion116@gmail.com>
2020-05-11 10:23:34 +0300

    changes to work well with frontend

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@ Y=2020
 M=5
 D=11
 i=23
-comment="getprofile, feed"
+comment="changes to work well with frontend"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
