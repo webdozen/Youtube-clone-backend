cfbb3ae5da4fc7a9809be7ab88ecc942e4bcff1e
Webdozen <microlion116@gmail.com>
2020-05-11 10:23:34 +0300

    added likedvideos, user query

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@ Y=2020
 M=5
 D=11
 i=23
-comment="changes to work well with frontend"
+comment="added likedvideos, user query"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
