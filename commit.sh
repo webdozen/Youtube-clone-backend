2f14d5c15fc5057e6583926973a3dd3d8e712e68
Webdozen <microlion116@gmail.com>
2020-05-15 10:23:34 +0300

    default avatar url

--- commit.sh
+++ commit.sh
@@ -1,8 +1,8 @@
 Y=2020
 M=5
-D=14
+D=15
 i=23
-comment="heroku add postgres"
+comment="default avatar url"
 
 export GIT_COMMITTER_DATE="$Y-$M-$D 10:$i:34"
 export GIT_AUTHOR_DATE="$Y-$M-$D 10:$i:34"
